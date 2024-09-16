db191618ff56916b3bd884d0d402cf0c1ff85626
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-09-16 17:36:07 +0200

    seo improvments

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
-
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
-import { headers } from "next/headers";
 
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
@@ -13,7 +11,7 @@ const corsHeaders = {
 
 export async function OPTIONS() {
     return NextResponse.json({}, { headers: corsHeaders });
-};
+}
 
 export async function POST(
     req: Request,
@@ -25,31 +23,29 @@ export async function POST(
         return new NextResponse("Product IDs are required", { status: 400 });
     }
 
+    // Fetch computers by IDs from the database
     const computers = await prismadb.computer.findMany({
         where: {
             id: {
-                in: computerIds
-            }
-        }
+                in: computerIds,
+            },
+        },
     });
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+    // Prepare line items for Stripe checkout session
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = computers.map(computer => ({
+        quantity: 1,
+        price_data: {
+            currency: 'EUR',
+            product_data: {
+                name: computer.name,
+            },
+            unit_amount: computer.price.toNumber() * 100, // Convert price to cents
+            tax_behavior: 'inclusive',
+        },
+    }));
 
-    computers.forEach(computer => {
-        line_items.push({
-            quantity: 1,
-            price_data: {
-                currency: 'EUR',
-                product_data: {
-                    name: computer.name,
-                },
-                unit_amount: computer.price.toNumber() * 100,
-                tax_behavior: 'inclusive',
-                
-            }
-        })
-    });
-
+    // Create the order in the database
     const order = await prismadb.order.create({
         data: {
             storeId: params.storeId,
@@ -58,59 +54,52 @@ export async function POST(
                 create: computerIds.map((computerId: string) => ({
                     computer: {
                         connect: {
-                            id: computerId
-                        }
-                    }
-                }))
-            }
-        }
+                            id: computerId,
+                        },
+                    },
+                })),
+            },
+        },
     });
 
-    const referer = headers().get("referer");
+    // Get the referer header
+    const referer = req.headers.get("referer");
 
-    const success_url = () => {
+    // Generate success and cancel URLs based on referer
+    const success_url = referer?.includes(".nl")
+        ? `${process.env.FRONTEND_STORE1_URL}/cart?success=1`
+        : `${process.env.FRONTEND_STORE2_URL}/cart?success=1` || `${process.env.FRONTEND_STORE_DEFAULT_URL}/cart?success=1`;
 
-        if (referer) {
-            if (referer.includes(".nl")) {
-                return `${process.env.FRONTEND_STORE1_URL}/cart?success=1`;
-            } else if (referer.includes(".com")) {
-                return `${process.env.FRONTEND_STORE2_URL}/cart?success=1`;
-            }
-        }
+    const cancel_url = referer?.includes(".nl")
+        ? `${process.env.FRONTEND_STORE1_URL}/cart?canceled=1`
+        : `${process.env.FRONTEND_STORE2_URL}/cart?canceled=1` || `${process.env.FRONTEND_STORE_DEFAULT_URL}/cart?canceled=1`;
+
+    try {
+        // Create Stripe checkout session
+        const session = await stripe.checkout.sessions.create({
+            line_items,
+            mode: "payment",
+            billing_address_collection: "required",
+            consent_collection: {
+                terms_of_service: "required",
+            },
+            shipping_address_collection: {
+                allowed_countries: ["NL", "BE"],
+            },
+            phone_number_collection: {
+                enabled: true,
+            },
+            customer_creation: "always",
+            success_url: success_url,
+            cancel_url: cancel_url,
+            metadata: {
+                orderId: order.id,
+            },
+        });
+
+        // Return the Stripe session URL
+        return NextResponse.json({ url: session.url }, { headers: corsHeaders });
+    } catch (error) {
+        return new NextResponse(`Error creating Stripe session: ${error}`, { status: 500 });
     }
-
-    const cancel_url = () => {
-            if (referer) {
-                if (referer.includes(".nl")) {
-                    return `${process.env.FRONTEND_STORE1_URL}/cart?canceled=1`;
-                } else if (referer.includes(".com")) {
-                    return `${process.env.FRONTEND_STORE2_URL}/cart?canceled=1`;
-                }
-            }
-    }
-
-    const session = await stripe.checkout.sessions.create({
-        line_items,
-        mode: 'payment',
-        billing_address_collection: 'required',
-        consent_collection: {
-            terms_of_service: 'required',
-        },
-        shipping_address_collection: {
-            allowed_countries: ['NL', 'BE'],
-        },
-        phone_number_collection: {
-            enabled: true
-        },
-        customer_creation: "always",
-        success_url: `${success_url()}`,
-        cancel_url: `${cancel_url()}`,
-        metadata: {
-            orderId: order.id
-        }
-    });
-
-    return NextResponse.json({ url: session.url }, {
-        headers: corsHeaders
-    });
 }
