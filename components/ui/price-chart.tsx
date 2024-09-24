43cf5488c71321d355041eb3b931e9717892433d
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-09-24 15:11:27 +0200

    new styling and chart debugging

--- components/ui/price-chart.tsx
+++ components/ui/price-chart.tsx
@@ -5,14 +5,13 @@ import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Responsi
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Button } from "@/components/ui/button"
-import { Calendar as CalendarIcon } from "lucide-react"
+import { Calendar as CalendarIcon, TrendingDownIcon, TrendingUpIcon } from "lucide-react"
 import { format, subMonths, subYears, startOfDay, endOfDay, subWeeks } from "date-fns"
 import { Calendar } from "@/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { cn } from "@/lib/utils"
 import { DateRange } from "react-day-picker"
 import { ProductGraphData } from '@/types'
-
 
 interface PriceChartProps {
     productData: ProductGraphData[]
@@ -23,60 +22,75 @@ interface PriceChartProps {
 
 const PriceChart:React.FC<PriceChartProps> = ({ productData, ProductName, minPriceNumber, avgPriceNumber }) => {
     const [data, setData] = useState<typeof productData>([])
-        const [dateRange, setDateRange] = useState<DateRange | undefined>({
-            from: subYears(new Date(), 1),
-            to: new Date()
+    const [dateRange, setDateRange] = useState<DateRange | undefined>({
+        from: subYears(new Date(), 1),
+        to: new Date()
+    })
+    const [filterType, setFilterType] = useState('1y')
+    
+    useEffect(() => {
+        setData(productData)
+    }, [productData])
+    
+    const filterData = (start: Date | undefined, end: Date | undefined) => {
+        if (!start || !end || !data) return []
+        return data.filter(item => {
+            const itemDate = new Date(item.date)
+            return itemDate >= startOfDay(start) && itemDate <= endOfDay(end)
         })
-        const [filterType, setFilterType] = useState('1y')
+    }
+    
+    const handleFilterChange = (value: string) => {
+        setFilterType(value)
+        const end = new Date()
+        let start: Date
         
-        useEffect(() => {
-            setData(productData)
-        }, [productData])
-        
-        const filterData = (start: Date | undefined, end: Date | undefined) => {
-            if (!start || !end || !data) return []
-            return data.filter(item => {
-                const itemDate = new Date(item.date)
-                return itemDate >= startOfDay(start) && itemDate <= endOfDay(end)
-            })
+        switch (value) {
+            case '2w':
+                start = subWeeks(end, 2)
+                break
+            case '1m':
+                start = subMonths(end, 1)
+                break
+            case '6m':
+                start = subMonths(end, 6)
+                break
+            case '1y':
+                start = subYears(end, 1)
+                break
+            default:
+                start = subMonths(end, 1)
+                break
         }
         
-        const handleFilterChange = (value: string) => {
-            setFilterType(value)
-            const end = new Date()
-            let start: Date
-            
-            switch (value) {
-                case '2w':
-                    start = subWeeks(end, 2)
-                    break
-                case '1m':
-                    start = subMonths(end, 1)
-                    break
-                case '6m':
-                    start = subMonths(end, 6)
-                    break
-                case '1y':
-                    start = subYears(end, 1)
-                    break
-                default:
-                    start = subMonths(end, 1)
-                    break
-            }
-            
-            setDateRange({ from: start, to: end })
-        }
-        
-        const filteredData = filterData(dateRange?.from, dateRange?.to)
+        setDateRange({ from: start, to: end })
+    }
+    
+    const filteredData = filterData(dateRange?.from, dateRange?.to)
 
-        const yAxisMin = Math.floor(minPriceNumber / 2) 
-        const yAxisMax = Math.ceil(avgPriceNumber * 1.8)
-        
+    const yAxisMin = Math.min(...filteredData.map(item => item.minPrice), minPriceNumber)
+    const yAxisMax = Math.max(...filteredData.map(item => item.avgPrice), avgPriceNumber)
 
-        return (
+    const calculatePercentageChange = () => {
+        if (filteredData.length < 2) return 0
+        const firstPrice = filteredData[0].avgPrice
+        const lastPrice = filteredData[filteredData.length - 1].avgPrice
+        return ((lastPrice - firstPrice) / firstPrice) * 100
+    }
+
+    const percentageChange = calculatePercentageChange()
+    const isTrendingUp = percentageChange > 0
+    const trendColor = isTrendingUp ? 'text-red-500' : 'text-green-500'
+    const trendText = isTrendingUp ? 'up' : 'down'
+    const trendIcon = isTrendingUp ? `${<TrendingUpIcon className='h-4 w-4 text-red-500' strokeWidth={1.4} />}` : `${<TrendingDownIcon className="h-4 w-4 text-green-500" strokeWidth={1.4} />}`
+
+    return (
         <Card className="w-full max-w-[1200px] mx-auto">
             <CardHeader>
                 <CardTitle>{ProductName} Price Trends</CardTitle>
+                <div className={`text-lg font-semibold ${trendColor}`}>
+                    {`Price is trending ${trendText} by ${percentageChange.toFixed(2)}% ${trendIcon}`}
+                </div>
             </CardHeader>
             <CardContent>
                 <div className="flex space-x-4 mb-4">
@@ -93,39 +107,39 @@ const PriceChart:React.FC<PriceChartProps> = ({ productData, ProductName, minPri
                         </SelectContent>
                     </Select>
                     {filterType === 'custom' && (
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <Button
-                            variant={"outline"}
-                            className={cn(
-                            "w-[280px] justify-start text-left font-normal",
-                            !dateRange && "text-muted-foreground"
-                            )}
-                            >
-                            <CalendarIcon className="mr-2 h-4 w-4" />
-                            {dateRange?.from ? (
-                                dateRange.to ? (
-                                <>
-                                {format(dateRange.from, "LLL dd, y")} -{" "}
-                                {format(dateRange.to, "LLL dd, y")}
-                                </>
-                                ) : (
-                                format(dateRange.from, "LLL dd, y")
-                                )
-                                ) : (
-                                <span>Pick a date</span>
-                                )}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0">
-                            <Calendar
-                            mode="range"
-                            selected={dateRange}
-                            onSelect={setDateRange}
-                            initialFocus
-                            />
-                        </PopoverContent>
-                    </Popover>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[280px] justify-start text-left font-normal",
+                                        !dateRange && "text-muted-foreground"
+                                    )}
+                                >
+                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                    {dateRange?.from ? (
+                                        dateRange.to ? (
+                                            <>
+                                                {format(dateRange.from, "LLL dd, y")} -{" "}
+                                                {format(dateRange.to, "LLL dd, y")}
+                                            </>
+                                        ) : (
+                                            format(dateRange.from, "LLL dd, y")
+                                        )
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0">
+                                <Calendar
+                                    mode="range"
+                                    selected={dateRange}
+                                    onSelect={setDateRange}
+                                    initialFocus
+                                />
+                            </PopoverContent>
+                        </Popover>
                     )}
                 </div>
                 <div className="w-full h-[500px]">
@@ -133,23 +147,23 @@ const PriceChart:React.FC<PriceChartProps> = ({ productData, ProductName, minPri
                         <LineChart data={filteredData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                             <CartesianGrid strokeDasharray="3 3" />
                             <XAxis 
-                            dataKey="date" 
-                            tickFormatter={(tick) => format(new Date(tick), 'dd MMM')}
+                                dataKey="date" 
+                                tickFormatter={(tick) => format(new Date(tick), 'dd MMM')}
                             />
                             <YAxis 
                                 domain={[yAxisMin, yAxisMax]}
                                 tickFormatter={(value) => `€${value}`}
                             />
                             <Tooltip labelFormatter={(label) => format(new Date(label), 'dd-MM-yyyy')} />
-                                <Legend />
-                                <Line type="monotone" dataKey="avgPrice" stroke="#ff2c2c" name="Average Price" />
-                                <Line type="monotone" dataKey="minPrice" stroke="#32cd32" name="Minimum Price" />
-                            </LineChart>
-                        </ResponsiveContainer>
-                    </div>
-                </CardContent>
-            </Card>
-            )
-        }
+                            <Legend />
+                            <Line type="monotone" dataKey="avgPrice" stroke="#ff2c2c" name="Average Price" />
+                            <Line type="monotone" dataKey="minPrice" stroke="#32cd32" name="Minimum Price" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
 
 export default PriceChart
