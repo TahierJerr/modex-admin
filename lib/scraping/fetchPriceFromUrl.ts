94b01df211e5efc833b9bc9f145c1a29404d1c06
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-09-24 13:26:37 +0200

    Update fetchPriceFromUrl.ts

--- lib/scraping/fetchPriceFromUrl.ts
+++ lib/scraping/fetchPriceFromUrl.ts
@@ -10,70 +10,72 @@ export async function fetchPriceFromUrl(url: string) {
         const { data } = await axios.get(url);
         console.log(url)
         const $ = cheerio.load(data);
-
+        
         let productPrice = '';
         let productUrl = '';
         let productUri = '';
-
+        
         const priceElement = $('td.shop-price').first();
-
+        
         if (priceElement.length) {
             productPrice = priceElement.text().trim();
         }
-
+        
         const linkElement = priceElement.find('a');
-
+        
         if (!productPrice) {
             throw new Error("Price not found.");
         }
-
+        
         if (linkElement.length) {
             productUrl = linkElement.attr('href') || '';
         }
-
+        
         if (!productUrl) {
             throw new Error("Url not found.");
         }
-
-        const uriElement = $('.priceHistoryGraph').first();
-
+        
+        const uriElement = $('twk-price-history-graph#priceHistoryGraph').first();
+        
         if (uriElement.length) {
             productUri = uriElement.attr('optionsrc') || '';
         }
-
+        
         if (!productUri) {
             throw new Error("Uri not found.");
         }
 
+        console.log("URI Element:", uriElement.html());
+        
         const productGraphData: ProductGraphData[] = await fetchChartData(productUri)
-
+        
         console.log(productGraphData)
-
+        
         const prices: number[] = [];
         $('.shop-price').each((index, element) => {
             const priceText = $(element).text().trim();
             const price = parseFloat(
                 priceText
-                    .replace(/€|\s/g, '')
-                    .replace(/,/g, '.')
-                    .replace(/-/g, '00')
+                .replace(/€|\s/g, '')
+                .replace(/,/g, '.')
+                .replace(/-/g, '00')
             );
             if (!isNaN(price)) {
                 prices.push(price);
             }
         });
-
+        
         const productAvgPrice = prices.length
-            ? (prices.reduce((sum, price) => sum + price, 0) / prices.length).toString()
-            : parseFloat(productPrice).toString();
-
+        ? (prices.reduce((sum, price) => sum + price, 0) / prices.length).toString()
+        : parseFloat(productPrice).toString();
+        
         if (!productPrice) {
             throw new Error("Unable to scrape product data");
         }
-
+        
         const formattedMinPrice = formatPrice(parseFloat(productPrice));
         const formattedAvgPrice = formatPrice(parseFloat(productAvgPrice));
-
+        
         const productData = {
             minPriceNumber: priceToNumber(productPrice),
             avgPriceNumber: priceToNumber(productAvgPrice),
@@ -81,10 +83,10 @@ export async function fetchPriceFromUrl(url: string) {
             avgPrice: formattedAvgPrice,
             productUrl,
         };
-
+        
         console.log(`Product Data: ${productData}`)
         console.log(`Product Graph Data: ${productGraphData}`)
-
+        
         return {
             ...productData,
             productGraphData
