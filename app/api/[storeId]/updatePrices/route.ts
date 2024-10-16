1249e3a4d768c6f468cfbaf75a779d96a5207b80
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-10-17 00:45:42 +0200

    callback

--- app/api/[storeId]/updatePrices/route.ts
+++ app/api/[storeId]/updatePrices/route.ts
@@ -1,6 +1,6 @@
 export const maxDuration = 60;
 
-import { updateGraphicsCardPrices } from "@/lib/functions/graphics/updateGrapicsPrices";
+import { updateGraphicsCardPrices } from "@/lib/functions/updateProductPrice";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { storeId: string } }) {
