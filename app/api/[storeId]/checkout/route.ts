557366c8d4c5f7f8c211a6ef8bc5f33ba9e51481
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-10-21 10:19:22 +0200

    adding model user and expaning model order and changing checout api

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -14,10 +14,14 @@ export async function OPTIONS() {
     return NextResponse.json({}, { headers: corsHeaders });
 };
 
-export async function POST(
-    req: Request,
-    { params }: { params: { storeId: string } }
-) {
+export async function POST(req: Request, { params }: { params: { storeId: string } }) {
+    const url = new URL(req.url);
+    const userId = url.searchParams.get('userId');
+
+    if (!userId) {
+        return new NextResponse("User ID is required", { status: 400 });
+    }
+
     const { computerIds } = await req.json();
 
     if (!computerIds || computerIds.length === 0) {
@@ -32,25 +36,40 @@ export async function POST(
         }
     });
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
     computers.forEach(computer => {
-        line_items.push({
-            quantity: 1,
-            price_data: {
-                currency: 'EUR',
-                product_data: {
-                    name: computer.name,
-                },
-                unit_amount: Math.round(parseFloat(computer.price.toFixed(2)) * 100),
-                tax_behavior: 'inclusive',
-            }
-        });
+        if (!computer) {
+            return new NextResponse("Product not found", { status: 404 });
+        }
     });
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = computers.map(computer => ({
+        quantity: 1,
+        price_data: {
+            currency: 'EUR',
+            product_data: {
+                name: computer.name,
+            },
+            unit_amount: Math.round(parseFloat(computer.price.toFixed(2)) * 100),
+            tax_behavior: 'inclusive',
+        }
+    }));
+
+    const totalPrice = computers.reduce((acc, computer) => acc + parseFloat(computer.price.toFixed(2)), 0);
+
+    const user = await prismadb.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+
+    if (!user) {
+        return new NextResponse("User not found", { status: 404 });
+    }
 
     const order = await prismadb.order.create({
         data: {
             storeId: params.storeId,
+            userId: userId,
             isPaid: false,
             orderItems: {
                 create: computerIds.map((computerId: string) => ({
@@ -60,21 +79,27 @@ export async function POST(
                         }
                     }
                 }))
-            }
+            },
+            phone: user.phone || "",
+            address: "",
+            postalCode: "",
+            country: "",
+            email: user.email,
+            orderStatus: "Pending",
+            paymentMethod: "",
+            totalPrice: totalPrice,
         }
     });
 
     const origin = req.headers.get("origin");
 
     if (!origin) {
-        console.log(origin);
         return new NextResponse("Origin header is required", { status: 400 });
     }
 
     const success_url = `${origin}/cart?success=1`;
     const cancel_url = `${origin}/cart?canceled=1`;
 
-    
     const session = await stripe.checkout.sessions.create({
         line_items,
         mode: 'payment',
@@ -89,8 +114,8 @@ export async function POST(
             enabled: true
         },
         customer_creation: "always",
-        success_url: `${success_url}`,
-        cancel_url: `${cancel_url}`,
+        success_url,
+        cancel_url,
         metadata: {
             orderId: order.id
         }
